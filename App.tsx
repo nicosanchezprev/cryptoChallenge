f0e87735823c521b626fdf438ff2823bc91eb5d2
Nicolas Sanchez <106752007+nicosanchezprev@users.noreply.github.com>
2023-02-20 12:38:20 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-// import { Provider } from 'react-redux';
 
 import Criptotracker from './components/Criptotracker/Criptotracker';
 
 function App(): JSX.Element {
   return (
-    // <Provider store={}>
     <Criptotracker />
-    // </Provider>
   );
 }
 
