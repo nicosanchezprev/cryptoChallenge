2ff2d2e91213ece29d06ab8be494dadd9d0fc22c
Nicolas Sanchez <106752007+nicosanchezprev@users.noreply.github.com>
2023-02-20 12:43:58 -0300

    Update Criptotracker-Styles.ts

--- components/Criptotracker/Criptotracker-Styles.ts
+++ components/Criptotracker/Criptotracker-Styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 
 import { theme } from '../../utils/theme';
 
-export const Principalview = styled.SafeAreaView`
+export const PrincipalView = styled.SafeAreaView`
   background-color: ${theme.colors.white};
   flex: 1;
 `;
